bba558050d6ef30e92285d995145119e5affa0aa
Cezary Piątek <cezarypiatek@live.com>
2023-04-13 22:41:24 +0200

    Fix layout and scrolling

---
+++ src/WireMockAdminUI/Views/RequestList.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace WireMockAdminUI.Views
+{
+    public partial class RequestList : UserControl
+    {
+        public RequestList()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ src/WireMockAdminUI/Views/RequestListFilters.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace WireMockAdminUI.Views
+{
+    public partial class RequestListFilters : UserControl
+    {
+        public RequestListFilters()
+        {
+            InitializeComponent();
+        }
+    }
+}
