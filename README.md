50aa61111962531859e8d4ffef5eeb917712cef4
Cezary Piątek <cezarypiatek@live.com>
2023-05-06 12:34:46 +0200

    Add package with extension method for WireMock.Net

--- README.md
+++ README.md
@@ -20,3 +20,26 @@ After installation you can easily run the app by executing `wiremockinspector` c
 - Presents a list of all available mappings with the definition
 
 ![](wiremock_basic_features.gif)
+
+
+## Using WireMockInspector from test code
+
+1. Install [WireMockInspector](https://github.com/WireMock-Net/WireMockInspector) dotnet tool
+
+	```shell
+	dotnet tool install WireMockInspector --global --no-cache --ignore-failed-sources
+	```
+
+2. Install `WireMock.Net.Extensions.WireMockInspector` nuget packet to your test project
+3. Example usage in the code
+
+	```cs
+	using var wireMock = WireMockServer.Start(new WireMockServerSettings()
+						{
+							StartAdminInterface = true,
+							Port = 9095
+						});
+
+	// Call Inspect() run WireMockInspect and attach it to current WireMockServer instance
+	wireMock.Inspect();
+	```
---
+++ src/WireMock.Net.Extensions.WireMockInspector/WireMockServerExtensions.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Diagnostics;
+using System.Runtime.CompilerServices;
+using WireMock.Server;
+
+namespace WireMock.Net.Extensions.WireMockInspector;
+
+public static class WireMockServerExtensions
+{
+    /// <summary>
+    ///     Run WireMockInspector dotnet tool and attach it to the current WireMockServer instance
+    /// </summary>
+    /// <remarks>
+    ///     The current thread will be blocked until WireMockInspector process exits.
+    /// </remarks>
+    public static void Inspect(this IWireMockServer wireMock, [CallerMemberName] string callerMethodName = "")
+    {
+        if (wireMock.IsStarted == false)
+            throw new InvalidOperationException("WireMock server is not started");
+
+        var inspectorProcess = Process.Start(new ProcessStartInfo
+        {
+            FileName = "wiremockinspector",
+            Arguments = $"attach --adminUrl {wireMock.Url} --autoLoad --instanceName {callerMethodName}",
+            UseShellExecute = false
+        });
+
+        inspectorProcess?.WaitForExit();
+    }
+}
