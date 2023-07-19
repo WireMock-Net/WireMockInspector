1f110c12fdd0217a08683f7acdc386fd503717f1
Cezary Piątek <cezarypiatek@live.com>
2023-07-19 23:06:52 +0200

    Add main tab with scenarios

---
+++ src/WireMockInspector/Views/ScenarioPage.axaml.cs
@@ -0,0 +1,18 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace WireMockInspector.Views;
+
+public partial class ScenarioPage : UserControl
+{
+    public ScenarioPage()
+    {
+        InitializeComponent();
+    }
+
+    private void InitializeComponent()
+    {
+        AvaloniaXamlLoader.Load(this);
+    }
+}
