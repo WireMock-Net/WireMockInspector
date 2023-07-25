3dd5bac58c4b295ba164863cb5b47cde99238a0b
Cezary Piątek <cezarypiatek@live.com>
2023-07-25 18:37:32 +0200

    Update Avalonia to 11.0.0

--- src/WireMockInspector/Views/MainWindow.axaml.cs
+++ src/WireMockInspector/Views/MainWindow.axaml.cs
@@ -1,16 +1,13 @@
 using System.Reactive.Disposables;
 using System.Reactive.Linq;
 using Avalonia.ReactiveUI;
-using MessageBox.Avalonia.Enums;
 using ReactiveUI;
 using WireMockInspector.ViewModels;
 using System;
-using System.Collections.Generic;
 using System.Reflection;
-using System.Threading.Tasks;
 using Avalonia.Controls;
-using WireMock.Admin.Mappings;
-using WireMock.Admin.Requests;
+using MsBox.Avalonia;
+using MsBox.Avalonia.Enums;
 
 
 namespace WireMockInspector.Views
@@ -30,25 +27,25 @@ namespace WireMockInspector.Views
                 ViewModel!.LoadRequestsCommand
                     .ThrownExceptions
                     .ObserveOn(RxApp.MainThreadScheduler)
-                    .Subscribe(_ =>
+                    .Subscribe(async _ =>
                     {
-                        var msg = MessageBox.Avalonia.MessageBoxManager.GetMessageBoxStandardWindow("Error",
+                        var msg = MessageBoxManager.GetMessageBoxStandard("Error",
                             "Cannot connect to WireMock Admin API.\r\n\r\nMake sure that:\r\n- The URL is correct\r\n- WireMock is started with Admin API enabled", ButtonEnum.Ok,
-                            MessageBox.Avalonia.Enums.Icon.Error, WindowStartupLocation.CenterOwner);
-                        msg.ShowDialog(this);
+                            MsBox.Avalonia.Enums.Icon.Error, WindowStartupLocation.CenterOwner);
+                       await msg.ShowAsync();
                     }).DisposeWith(disposables);
-
+            
                 ViewModel.SaveServerSettings
                     .ThrownExceptions
                     .ObserveOn(RxApp.MainThreadScheduler)
-                    .Subscribe(ex =>
+                    .Subscribe(async ex =>
                     {
-                        var msg = MessageBox.Avalonia.MessageBoxManager.GetMessageBoxStandardWindow("Error",
+                        var msg =   MessageBoxManager.GetMessageBoxStandard("Error",
                             $"Cannot save server settings.\r\n\r\n {ex.Message}", ButtonEnum.Ok,
-                            MessageBox.Avalonia.Enums.Icon.Error, WindowStartupLocation.CenterOwner);
-                        msg.ShowDialog(this);
+                            MsBox.Avalonia.Enums.Icon.Error, WindowStartupLocation.CenterOwner);
+                        await msg.ShowAsync();
                     }).DisposeWith(disposables);
-
+            
                 if (Settings != null)
                 {
                     ViewModel.AdminUrl = Settings.AdminUrl;
@@ -56,7 +53,7 @@ namespace WireMockInspector.Views
                     {
                       ViewModel.LoadRequestsCommand.Execute().Subscribe().DisposeWith(disposables);
                     }
-
+            
                     if (string.IsNullOrWhiteSpace(Settings.InstanceName) == false)
                     {
                         SetInstanceName(Settings.InstanceName);
