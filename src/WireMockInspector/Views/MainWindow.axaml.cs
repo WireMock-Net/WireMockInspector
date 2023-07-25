e26a9b7c0a43ac7368db9f58137da54e2c9e1631
Cezary Piątek <cezarypiatek@live.com>
2023-07-25 18:37:32 +0200

    Update Avalonia to 11.0.0

--- src/WireMockInspector/Views/MainWindow.axaml.cs
+++ src/WireMockInspector/Views/MainWindow.axaml.cs
@@ -1,7 +1,6 @@
 using System.Reactive.Disposables;
 using System.Reactive.Linq;
 using Avalonia.ReactiveUI;
-using MessageBox.Avalonia.Enums;
 using ReactiveUI;
 using WireMockInspector.ViewModels;
 using System;
@@ -9,6 +8,8 @@ using System.Collections.Generic;
 using System.Reflection;
 using System.Threading.Tasks;
 using Avalonia.Controls;
+using MsBox.Avalonia;
+using MsBox.Avalonia.Enums;
 using WireMock.Admin.Mappings;
 using WireMock.Admin.Requests;
 
@@ -25,44 +26,44 @@ namespace WireMockInspector.Views
         {
             
             InitializeComponent();
-            this.WhenActivated((disposables) =>
-            {
-                ViewModel!.LoadRequestsCommand
-                    .ThrownExceptions
-                    .ObserveOn(RxApp.MainThreadScheduler)
-                    .Subscribe(_ =>
-                    {
-                        var msg = MessageBox.Avalonia.MessageBoxManager.GetMessageBoxStandardWindow("Error",
-                            "Cannot connect to WireMock Admin API.\r\n\r\nMake sure that:\r\n- The URL is correct\r\n- WireMock is started with Admin API enabled", ButtonEnum.Ok,
-                            MessageBox.Avalonia.Enums.Icon.Error, WindowStartupLocation.CenterOwner);
-                        msg.ShowDialog(this);
-                    }).DisposeWith(disposables);
-
-                ViewModel.SaveServerSettings
-                    .ThrownExceptions
-                    .ObserveOn(RxApp.MainThreadScheduler)
-                    .Subscribe(ex =>
-                    {
-                        var msg = MessageBox.Avalonia.MessageBoxManager.GetMessageBoxStandardWindow("Error",
-                            $"Cannot save server settings.\r\n\r\n {ex.Message}", ButtonEnum.Ok,
-                            MessageBox.Avalonia.Enums.Icon.Error, WindowStartupLocation.CenterOwner);
-                        msg.ShowDialog(this);
-                    }).DisposeWith(disposables);
-
-                if (Settings != null)
-                {
-                    ViewModel.AdminUrl = Settings.AdminUrl;
-                    if (Settings.AutoLoad)
-                    {
-                      ViewModel.LoadRequestsCommand.Execute().Subscribe().DisposeWith(disposables);
-                    }
-
-                    if (string.IsNullOrWhiteSpace(Settings.InstanceName) == false)
-                    {
-                        SetInstanceName(Settings.InstanceName);
-                    }
-                }
-            });
+            // this.WhenActivated((disposables) =>
+            // {
+            //     ViewModel!.LoadRequestsCommand
+            //         .ThrownExceptions
+            //         .ObserveOn(RxApp.MainThreadScheduler)
+            //         .Subscribe(async _ =>
+            //         {
+            //             var msg = MessageBoxManager.GetMessageBoxStandard("Error",
+            //                 "Cannot connect to WireMock Admin API.\r\n\r\nMake sure that:\r\n- The URL is correct\r\n- WireMock is started with Admin API enabled", ButtonEnum.Ok,
+            //                 MsBox.Avalonia.Enums.Icon.Error, WindowStartupLocation.CenterOwner);
+            //            await msg.ShowAsync();
+            //         }).DisposeWith(disposables);
+            //
+            //     ViewModel.SaveServerSettings
+            //         .ThrownExceptions
+            //         .ObserveOn(RxApp.MainThreadScheduler)
+            //         .Subscribe(async ex =>
+            //         {
+            //             var msg =   MessageBoxManager.GetMessageBoxStandard("Error",
+            //                 $"Cannot save server settings.\r\n\r\n {ex.Message}", ButtonEnum.Ok,
+            //                 MsBox.Avalonia.Enums.Icon.Error, WindowStartupLocation.CenterOwner);
+            //             await msg.ShowAsync();
+            //         }).DisposeWith(disposables);
+            //
+            //     if (Settings != null)
+            //     {
+            //         ViewModel.AdminUrl = Settings.AdminUrl;
+            //         if (Settings.AutoLoad)
+            //         {
+            //           ViewModel.LoadRequestsCommand.Execute().Subscribe().DisposeWith(disposables);
+            //         }
+            //
+            //         if (string.IsNullOrWhiteSpace(Settings.InstanceName) == false)
+            //         {
+            //             SetInstanceName(Settings.InstanceName);
+            //         }
+            //     }
+            // });
             _mainTitle = $"WireMockInspector v{Assembly.GetEntryAssembly()!.GetName().Version}";
             Title = _mainTitle;
         }
