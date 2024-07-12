9c372bf7ed8218092f612a7fa61930bf162c1e6f
Galadril <heinis.mark@gmail.com>
2024-07-12 23:49:12 +0200

    Refactor and enhance converter usage
    This commit represents a significant refactoring and enhancement of the converter usage within the WireMockInspector application. Key changes include the relocation of converter classes (`EnumItemsConverter`, `GraphConverter`, `StringListConverter`, `StringMatchConverter`) from the `WireMockInspector.ViewModels` namespace to a new `WireMockInspector.Converters` namespace, improving modularity and clarity. New converter classes have been added, encapsulating the conversion logic for better reusability. Updates in various XAML files reflect these namespace changes, ensuring correct converter references in data-binding expressions. Additionally, unused `using` directives have been cleaned up, and a new `SettingsWrapperTemplateSelector` class has been introduced to enhance dynamic UI generation capabilities. These changes collectively aim to improve the maintainability and functionality of the application's UI data presentation.

---
+++ src/WireMockInspector/Converters/StringListConverter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Globalization;
+using Avalonia.Data.Converters;
+
+namespace WireMockInspector.Converters;
+
+public class StringListConverter : IValueConverter
+{
+    public static readonly StringListConverter Instance = new();
+    public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        if (value is string[] sl)
+        {
+            return string.Join(",", sl);
+        }
+
+        return string.Empty;
+    }
+
+    public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        if (value is string s)
+        {
+            return s.Split(",");
+        }
+
+        return null;
+    }
+}
---
+++ src/WireMockInspector/Converters/StringMatchConverter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Globalization;
+using Avalonia.Data.Converters;
+
+namespace WireMockInspector.Converters;
+
+public class StringMatchConverter : IValueConverter
+{
+    public static readonly StringMatchConverter Instance = new();
+
+    public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        var parameterString = parameter?.ToString() ?? "null";
+        var valueString = value?.ToString() ?? "null";
+        return parameterString.Equals(valueString, StringComparison.InvariantCultureIgnoreCase);
+    }
+
+    public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        throw new NotImplementedException();
+    }
+}
